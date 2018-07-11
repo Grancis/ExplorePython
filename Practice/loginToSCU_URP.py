4bda8a62f63b4ca497f20083012fa680c5ad18ad
Grancis <wzhenchao@live.com>
2018-07-12 01:02:10 +0800

    改进登录川大教务处的模块

--- Practice/loginToSCU_URP.py
+++ Practice/loginToSCU_URP.py
@@ -10,10 +10,6 @@ import io
 from urllib import request,parse
 from http import cookiejar
 
-url='http://zhjw.scu.edu.cn/loginAction.do'
-xh=input('input your student ID: ')
-mm=input('input your password: ')
-data=[('zjh',xh),('mm',mm)]
 
 class LoginAgent(object):
     def __init__(self):
@@ -23,14 +19,20 @@ class LoginAgent(object):
         self.__https_handler=request.HTTPSHandler() #创建https管理器
         self.__agent=request.build_opener(self.__cookie_handler,self.__http_handler,self.__https_handler)
         self.__login_res=None
+        self.__url='http://zhjw.scu.edu.cn/loginAction.do'
+        self.__xh=None
+        self.__mm=None
 
     #login method 
     #para: self
     #return __agent with login status
-    def login(self,data):
+    def login(self):
+        self.__xh=input('Input your student ID: ')
+        self.__mm=input('Iuput your password: ')
+        data=[('zjh',self.__xh),('mm',self.__mm)]
         url_data=parse.urlencode(data)
         #post 请求创建
-        req=request.Request(url,data=url_data.encode('utf-8'))
+        req=request.Request(self.__url,data=url_data.encode('utf-8'))
         req.add_header('Origin','http://zhjw.scu.edu.cn')
         req.add_header('Referer','http://zhjw.scu.edu.cn/login.jsp')
         req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36') 
@@ -41,8 +43,11 @@ class LoginAgent(object):
     def getRes(self):
         return self.__res
 
-login_agent=LoginAgent()
-agent=login_agent.login(data)
+
+# moduel test
+if __name__=='__main__':
+    login_agent=LoginAgent()
+agent=login_agent.login()
 res=login_agent.getRes()
 
 for k,v in res.getheaders():
